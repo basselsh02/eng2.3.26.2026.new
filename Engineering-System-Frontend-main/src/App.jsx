2acd836f203bc238be42e44fc60ac26b5e41325b
alharonywaled-ui <alharonywaled@gmail.com>
2026-03-27 15:08:41 +0200

    Add missing office sections to sidebar and routes

--- Engineering-System-Frontend-main/src/App.jsx
+++ Engineering-System-Frontend-main/src/App.jsx
@@ -21,6 +21,22 @@ import TasjilByanAlMashro3 from "./components/pages/Oqood/TasjilByanAlMashro3";
 
 // مكتب الصيانة pages
 import SiyanaBayan from "./components/pages/Siyana/SiyanaBayan";
+import MohamatAlModirTawridat from "./components/pages/Tawridat/MohamatAlModirTawridat";
+import MohamatAlMowazafTawridat from "./components/pages/Tawridat/MohamatAlMowazafTawridat";
+import TasjilAlMawqifAlHali from "./components/pages/Tawridat/TasjilAlMawqifAlHali";
+import MutabaatAlTaswyat from "./components/pages/Tawridat/MutabaatAlTaswyat";
+import ByanAwamarTawrid from "./components/pages/Tawridat/ByanAwamarTawrid";
+import NamozhajDaribatAlMabiaat from "./components/pages/Tawridat/NamozhajDaribatAlMabiaat";
+import MutabaatAlTaqarir from "./components/pages/Tawridat/MutabaatAlTaqarir";
+import MohamatAlModirMashtarawat from "./components/pages/Mashtarawat/MohamatAlModirMashtarawat";
+import MohamatAlMowazafMashtarawat from "./components/pages/Mashtarawat/MohamatAlMowazafMashtarawat";
+import ByanatAlmashro3at from "./components/pages/Mashtarawat/ByanatAlmashro3at";
+import MahdarEjra2at from "./components/pages/Mashtarawat/MahdarEjra2at";
+import ByanAlTa3aqud from "./components/pages/Mizaniya/ByanAlTa3aqud";
+import TasjilAlMakhsamat from "./components/pages/Mizaniya/TasjilAlMakhsamat";
+import TasjilKhetabaatAlDaman from "./components/pages/Hesabat/TasjilKhetabaatAlDaman";
+import MutabaatDukholWaKhuroojAlMustakhlasat from "./components/pages/Hesabat/MutabaatDukholWaKhuroojAlMustakhlasat";
+import AlTaqarir from "./components/pages/Hesabat/AlTaqarir";
 
 import store from "./app/store";
 
@@ -56,6 +72,30 @@ const router = createBrowserRouter([
 
       // مكتب الصيانة - صفحات العمل
       { path: "siyana/bayan-mutawaqif", element: <SiyanaBayan /> },
+
+      // مكتب التوريدات
+      { path: "tawridat/mohamat-almodir", element: <MohamatAlModirTawridat /> },
+      { path: "tawridat/mohamat-almowazaf", element: <MohamatAlMowazafTawridat /> },
+      { path: "tawridat/tasjil-almawqif-alhali", element: <TasjilAlMawqifAlHali /> },
+      { path: "tawridat/mutabaat-altaswyat", element: <MutabaatAlTaswyat /> },
+      { path: "tawridat/byan-awamar-tawrid", element: <ByanAwamarTawrid /> },
+      { path: "tawridat/namozhaj-daribat-almabiaat", element: <NamozhajDaribatAlMabiaat /> },
+      { path: "tawridat/mutabaat-altaqarir", element: <MutabaatAlTaqarir /> },
+
+      // مكتب المشتريات
+      { path: "mashtarawat/mohamat-almodir", element: <MohamatAlModirMashtarawat /> },
+      { path: "mashtarawat/mohamat-almowazaf", element: <MohamatAlMowazafMashtarawat /> },
+      { path: "mashtarawat/byanat-almashro3at", element: <ByanatAlmashro3at /> },
+      { path: "mashtarawat/mahdar-ejra2at", element: <MahdarEjra2at /> },
+
+      // مكتب الميزانية
+      { path: "mizaniya/byan-alta3aqud", element: <ByanAlTa3aqud /> },
+      { path: "mizaniya/tasjil-almakhsamat", element: <TasjilAlMakhsamat /> },
+
+      // مكتب الحسابات
+      { path: "hesabat/tasjil-khetabaat-aldaman", element: <TasjilKhetabaatAlDaman /> },
+      { path: "hesabat/mutabaat-dukhol-khurooj-almustakhlasat", element: <MutabaatDukholWaKhuroojAlMustakhlasat /> },
+      { path: "hesabat/altaqarir", element: <AlTaqarir /> },
     ],
   },
 ]);
